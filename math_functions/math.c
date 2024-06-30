13954c4d8230af349de1ee244fae9c84f9d052e1
MarioGrusz <mario.gruszczynski@gmail.com>
2024-06-30 22:10:31 +0200

    new rep created lerning C language syntax

---
+++ math_functions/math.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double A  = sqrt(9); //lf
+    double B = pow(2, 4); //lf
+    int C = round(3.14); // d
+    int D = ceil(4.14); // d
+    int E = floor(3.99); //d
+
+    printf("\n%d\n", C);
+
+    return 0;
+}
