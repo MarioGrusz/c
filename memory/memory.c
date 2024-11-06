2913d9401ea35446d6023a6640c6f75fbe5111d1
MarioGrusz <mario.gruszczynski@gmail.com>
2024-11-06 23:42:04 +0100

    pointers in C

---
+++ memory/memory.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // memory = an array of bytes within RAM (street)
+    // memory block = a single unit (byte) within memory, used to hold some value (person)
+    // memory address = the address of where a memory block is located (house address)
+
+    return 0;
+}
+
+
+//MEMORY is like a street with bunch of different houses (blocks) and each block can store
+//some value. like house can store person, and the memory address is a house address
+
+//WHEN WE DECLARE VARIABLE WE SETTING SOME AMOUNT OF MEMORY BLOCK ASIDE (char a;) TO 
+//STORE SOME VALUE. THIS MEMORY BLOCK HAS ADDRESS 
+// char a = 'X
